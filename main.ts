b465a59f42db18463d5d17622ebcef6fbd4aea6a
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-03-12 19:24:27 -0400

    save

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ashlyn
+ * Created on: Mar 2026
+ * This program will show the birghtness level of light 
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+let lightAmount: number = 0
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+lightAmount = input.lightLevel()
+
+if (lightAmount <= 51) {
+    neopixelStrip.clear()
+}
+if (lightAmount > 52) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+}
+if (lightAmount > 104) {
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+}
+if (lightAmount > 156) {
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+}
+if (lightAmount > 208) {
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+}
+
+neopixelStrip.show()
+
