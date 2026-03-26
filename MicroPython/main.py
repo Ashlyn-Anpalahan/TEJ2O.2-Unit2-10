80d8f0535df94a4ddb9cb1ea6b66339414147f2d
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-03-26 18:27:27 +0000

    fixed python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,27 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Ashlyn
+Created on: Mar 2026
+This module will read the amount of light.
 """
 
 from microbit import *
+import neopixel
 
-display.scroll("Hello, World!")
+np = neopixel.NeoPixel(pin16, 4)
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.was_pressed():
+        light_amount = display.read_light_level()
+    np.clear()
+    if light_amount > 52:
+        np[0] = (255, 0, 0)
+    if light_amount > 104:
+        np[1] = (255, 0, 0)
+    if light_amount > 156:
+        np[2] = (255, 0, 0)
+    if light_amount > 208:
+        np[3] = (255, 0, 0)
+
+    np.show()
+    sleep(100)
